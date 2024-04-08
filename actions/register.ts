98d60b1e7ff9befb240093af4e3f0ce4970a8863
bipinkatuwal567 <bipinkatuwal098@gmail.com>
2024-04-08 16:06:28 +0545

    add prisma with neon(postgress), more functionalities to register

--- actions/register.ts
+++ actions/register.ts
@@ -1,12 +1,35 @@
+"use server";
 import { registerSchema } from "@/schemas";
 import * as z from "zod";
+import bcrypt from "bcryptjs";
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof registerSchema>) => {
   const validateRegister = registerSchema.safeParse(values);
 
-  if (!validateRegister) {
+  if (!validateRegister.success) {
     return { error: "Something went wrong" };
   }
 
-  return { success: "Email sent" };
+  const { email, password, name } = validateRegister.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "User already exist" };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  // TODO: Send verification token email
+
+  return { success: "User Created!" };
 };
